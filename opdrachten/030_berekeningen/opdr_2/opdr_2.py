3ef750f7be4a64d841a3ed594b015bf7ae0d8fd6
daniellevdo <97109861@st.deltion.nl>
2025-03-08 17:37:58 +0100

    Opdracht 3.1,3.2

--- opdrachten/030_berekeningen/opdr_2/opdr_2.py
+++ opdrachten/030_berekeningen/opdr_2/opdr_2.py
@@ -4,7 +4,21 @@
 
 # Hier komt je code...
 
-c = ...
-f = ...
+c = -12
+f = 102
 
-print()
+uitkomst1 = (c * 1.8) + 32
+uitkomst2 = (f - 32) / 1.8
+
+print(c, "graden Celsius staat gelijk aan", uitkomst1, "graden Fahrenheit")
+print(f, "graden Fahrenheit staat gelijk aan", uitkomst2, "graden Celsius")
+
+c = 62.2
+f = 96
+
+uitkomst3 = (c * 1.8) + 32
+uitkomst4 = (f - 32) / 1.8
+
+print(c, "graden Celsius staat gelijk aan", uitkomst3, "graden Fahrenheit")
+print(f, "graden Fahrenheit staat gelijk aan", uitkomst4, "graden Celsius")
+
--- opdrachten/030_berekeningen/opdr_3/opdr_3.py
+++ opdrachten/030_berekeningen/opdr_3/opdr_3.py
@@ -3,9 +3,12 @@
 # Groep:
 
 # Hier komt je code...
+
+
+
 x = 1
-y = ...
-print( ... )
+y = 4 x<sup>3</sup> - 2 1<sup>2</sup> - 1
+print(y)
 
 x = 2
 y = ...
