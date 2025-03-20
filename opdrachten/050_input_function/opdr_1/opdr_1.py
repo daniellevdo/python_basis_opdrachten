e0437fa09028245f5bdecb28fc42fa0618c5b07c
daniellevdo <97109861@st.deltion.nl>
2025-03-20 16:17:00 +0100

    Opdracht 5.1, 5.2

--- opdrachten/050_input_function/opdr_1/opdr_1.py
+++ opdrachten/050_input_function/opdr_1/opdr_1.py
@@ -4,4 +4,21 @@
 
 # Hier komt je code, maak gebruik van de input functie om de lengte van de rechthoekzijden van de driehoek op te vragen.
 
+#> Geef de lengte van de eerste zijde  
+#> 4  
+#> Geef de lengte van de tweede zijde  
+#> 3  
+#> 
+#> De lengte van de schuine zijde is: 5
+
+import math
+
+
+zijde1 = int(input("Geef de lengte van de eerste zijde \n"))
+zijde2 = int(input("Geef de lengte van de tweede zijde \n"))
+
+zijde3 = int((zijde1*zijde1 + zijde2*zijde2))
+zijde4 = math.sqrt(zijde3)
+print ("De lengte van de schuine zijde is: ")
+print (zijde4)
 
--- opdrachten/050_input_function/opdr_2/opdr_2.py
+++ opdrachten/050_input_function/opdr_2/opdr_2.py
@@ -4,4 +4,25 @@
 
 # Hier komt je code...
 
-gasten = ["Jij", ... ]
+x = True
+gastenlijst = ["Jij",  ]
+while x: 
+    gast = str(input("Noem een gast: \n"))
+    gastenlijst.append (gast)
+    if gast == "x":
+      x = False
+    if gast == "Marie":
+      gastenlijst.remove ("Marie")
+    #if gast == "George":
+      #gastenlijst.insert (0,"Kees")  
+
+    if gast == "George":
+            gastenlijst.remove("George")  # Eerst "George" tijdelijk verwijderen
+            
+            if "Kees" in gastenlijst:
+                gastenlijst.insert(gastenlijst.index("Kees") + 1, "George")  # Zet George ná Kees
+            else:
+                gastenlijst.append("Kees")  # Voeg Kees toe als hij er nog niet is
+                gastenlijst.append("George")  # Voeg George direct daarna toe
+print (gastenlijst)
+
