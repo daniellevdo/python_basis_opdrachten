3d982a538307a2ddc7f629db01a884f41aa7de5e
daniellevdo <97109861@st.deltion.nl>
2025-03-22 17:53:33 +0100

    Opdracht 5.3

--- opdrachten/050_input_function/opdr_3/opdr_3.py
+++ opdrachten/050_input_function/opdr_3/opdr_3.py
@@ -4,4 +4,26 @@
 
 # Hier komt je code...
 
+stedenlijst = []
+#while True:
+ #   stad = input("Geef een naam van een stad:\n")
+  #  if stad == "x":
+   #   break
+
+    #stedenlijst.append (stad)
+#print (stedenlijst)
+
+while True:
+    stad = input("Geef een naam van een stad:\n").strip()
+    
+    if stad.lower() == "x":  # Voorkomt dat 'X' of ' x ' problemen veroorzaakt
+        break
+
+    stedenlijst.append(stad)
+
+# Sorteer de lijst van Z naar A
+stedenlijst_sorted = sorted(stedenlijst, reverse=True)
+
+print(stedenlijst_sorted)
+
 
