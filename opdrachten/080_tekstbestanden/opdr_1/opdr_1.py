2a270afb870a01f163ba2abc0b2197fe041704cd
daniellevdo <97109861@st.deltion.nl>
2025-03-24 20:48:50 +0100

    Opdracht 8.1, 8.2

--- opdrachten/080_tekstbestanden/opdr_1/opdr_1.py
+++ opdrachten/080_tekstbestanden/opdr_1/opdr_1.py
@@ -2,4 +2,20 @@
 # Naam student:
 # Groep:
 
-# Jouw code komt hier
+
+prompt = ("Wat vind je van de huidige regering?\n\
+Wat vind je van de Python-lessen tot nu toe?\n\
+Wat vind jij de mooiste stad van Nederland?\n")
+
+active = True
+lst = []
+while active:
+    message = input(prompt)
+    if message == 'q':
+        active = False
+        fo = open('enquete.txt', 'wt')
+        for i in lst:
+            fo.write(i + "\n")
+        fo.close()
+    else:
+        lst.append (message)    
--- opdrachten/080_tekstbestanden/opdr_2/opdr_2.py
+++ opdrachten/080_tekstbestanden/opdr_2/opdr_2.py
@@ -5,6 +5,16 @@
 import random
 prompt = "Raad mijn geheime getal \n"
 geheim_getal = random.randint(1, 100)
+pogingen = 0
 
-# De rest moet jij doen.....
-
+active = True
+while active:
+  message = int(input(prompt))
+  pogingen += 1
+  if message < geheim_getal:
+      print("hoger")
+  if message > geheim_getal:
+      print ("lager")
+  if message == geheim_getal:
+      print (f"Je hebt het getal in {pogingen} pogingen geraden, het is {geheim_getal} !")
+      break
