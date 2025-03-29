089cc63b00dce6c7393552ab24f99fcb532ef9c1
daniellevdo <97109861@st.deltion.nl>
2025-03-29 21:09:49 +0100

    Opdracht 8.3, 8.4

--- opdrachten/080_tekstbestanden/opdr_3/opdr_3.py
+++ opdrachten/080_tekstbestanden/opdr_3/opdr_3.py
@@ -3,4 +3,34 @@
 # Groep:
 
 
+vraag = "Geef me een geheim\n" \
+        "vul een 'q' in om te stoppen\n" 
+
+while True:
+  geheim = input(vraag)
+
+  if geheim == 'q':
+    break
+
+  geheimtaal = ""
+
+  for char in geheim:
+      if char.isalpha():
+        shift = 5
+      
+        if char.islower():
+          new_char = chr(((ord(char) - ord('a') + shift) % 26) + ord('a'))
+        elif char.isupper():
+                new_char = chr(((ord(char) - ord('A') + shift) % 26) + ord('A'))
+        geheimtaal += new_char
+      else:
+        geheimtaal = char
+
+  print("Versleuteld geheim:", geheimtaal)
+ 
+
+
+
+
+
 
--- opdrachten/080_tekstbestanden/opdr_4/opdr_4.py
+++ opdrachten/080_tekstbestanden/opdr_4/opdr_4.py
@@ -4,3 +4,24 @@
 
 
 # Party enquete
+
+vraag1 = ("Wat is je voornaam?\n")
+vraag2 = ("Wat is je achternaam?\n")
+vraag3 = ("Wat neem je aan drank mee?\n")
+vraag4 = ("Wat neem je mee om te eten?\n")
+
+antwoordenlijst = []
+
+antwoord1 = input(vraag1)
+antwoord2 = input(vraag2)
+antwoord3 = input(vraag3)
+antwoord4 = input(vraag4)
+antwoordenlijst.append (antwoord1)    
+antwoordenlijst.append (antwoord2)    
+antwoordenlijst.append (antwoord3)    
+antwoordenlijst.append (antwoord4)    
+
+with open('party-info.txt', 'wt') as fo:
+    for antwoord in antwoordenlijst:
+        fo.write(antwoord + "\n")
+    
