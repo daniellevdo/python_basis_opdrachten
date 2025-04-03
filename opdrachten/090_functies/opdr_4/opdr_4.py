ae2df3a3e6d0f23e6f06454b03255ef1b47a96cc
daniellevdo <97109861@st.deltion.nl>
2025-04-03 14:27:44 +0200

    opdracht 9.4

--- opdrachten/090_functies/opdr_4/opdr_4.py
+++ opdrachten/090_functies/opdr_4/opdr_4.py
@@ -1,20 +1,17 @@
-# Opdracht 1 functies
-# Naam student:
-# Groep:
 
-lijst_met_namen = []
 def volledige_naam(lijst_met_namen):
-    lijst_met_namen.append(namen.title().rstrip().strip())
-    if tussenvoegsel == "":
-        print({voornaam}, (achternaam))
-    else:
-        print()
+    for naam in lijst_met_namen:
+        if naam["tussenvoegsel"]:
+            print(f"{naam['voornaam']} {naam['tussenvoegsel']} {naam['achternaam']}")
+        else:
+            print(f"{naam['voornaam']} {naam['achternaam']}")
 
 namen = [
-    {"voornaam": {voornaam=}"Willem", "tussenvoegsel": "van", "achternaam": "Dijk"},
+    {"voornaam": "Willem", "tussenvoegsel": "van", "achternaam": "Dijk"},
     {"voornaam": "Klaas", "tussenvoegsel": "", "achternaam": "Wopstra"},
     {"voornaam": "Miep", "tussenvoegsel": "van der", "achternaam": "Plas"},
     {"voornaam": "Carla", "tussenvoegsel": "", "achternaam": "Hoogvliet"},
 ]
 
 volledige_naam(namen)
+
